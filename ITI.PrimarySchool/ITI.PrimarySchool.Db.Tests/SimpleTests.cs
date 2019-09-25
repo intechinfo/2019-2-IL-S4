58486e5850f9a432992cc33dcf6940bd305ccaf3
Antoine Raquillet <antoine.raquillet@esiea.fr>
2019-09-25 11:57:06 +0200

    Added some tests.

--- ITI.PrimarySchool/ITI.PrimarySchool.Db.Tests/SimpleTests.cs
+++ ITI.PrimarySchool/ITI.PrimarySchool.Db.Tests/SimpleTests.cs
@@ -34,5 +34,49 @@ namespace ITI.PrimarySchool.Db.Tests
                 }
             }
         }
+
+        [Test]
+        public void insert_new_teacher()
+        {
+            using(SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                string firstName = GetRandomName();
+                string lastName = GetRandomName();
+                string cmd = $@"insert into ps.tTeacher(FirstName,  LastName)
+                                                 values(@FirstName, @LastName);";
+
+                using(SqlCommand command = new SqlCommand(cmd, conn))
+                {
+                    command.Parameters.AddWithValue("@FirstName", firstName);
+                    command.Parameters.AddWithValue("@LastName", lastName);
+                    command.ExecuteNonQuery();
+                }
+
+                cmd = $@"select t.TeacherId
+                         from ps.tTeacher t
+                         where t.FirstName = @FirstName
+                           and t.LastName = @LastName;";
+
+                int teacherId;
+                using (SqlCommand command = new SqlCommand(cmd, conn))
+                {
+                    command.Parameters.AddWithValue("@FirstName", firstName);
+                    command.Parameters.AddWithValue("@LastName", lastName);
+                    object id = command.ExecuteScalar();
+                    Assert.That(id, Is.Not.Null);
+                    teacherId = (int)id;
+                }
+
+                cmd = $"delete from ps.tTeacher where TeacherId = @TeacherId;";
+                using(SqlCommand command = new SqlCommand(cmd, conn))
+                {
+                    command.Parameters.AddWithValue("@TeacherId", teacherId);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        static string GetRandomName() => string.Format("Test-{0}", Guid.NewGuid().ToString().Substring(0, 16));
     }
 }
