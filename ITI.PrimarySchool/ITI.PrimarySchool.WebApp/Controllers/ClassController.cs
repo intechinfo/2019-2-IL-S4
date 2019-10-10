f76e11a6624cd87e831035bd9f41e31e73f3c33a
Antoine Raquillet <antoine.raquillet@esiea.fr>
2019-10-10 10:45:07 +0200

    Find class by id.

--- ITI.PrimarySchool/ITI.PrimarySchool.WebApp/Controllers/ClassController.cs
+++ ITI.PrimarySchool/ITI.PrimarySchool.WebApp/Controllers/ClassController.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using ITI.PrimarySchool.DAL;
 using Microsoft.AspNetCore.Mvc;
@@ -9,11 +8,16 @@ namespace ITI.PrimarySchool.WebApp.Controllers
 {
     public class ClassController : Controller
     {
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> All()
         {
             ClassGateway gateway = new ClassGateway();
-            ViewBag.Classes = await gateway.GetAll();
-            return View();
+            IEnumerable<ClassData> viewModel = await gateway.GetAll();
+            return View(viewModel);
+        }
+
+        public async Task<IActionResult> Index(int id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
