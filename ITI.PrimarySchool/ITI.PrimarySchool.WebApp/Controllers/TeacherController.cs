de581fc76933923aaa8ba7edc4534d37ac0daa33
Antoine Raquillet <antoine.raquillet@esiea.fr>
2019-10-10 12:02:34 +0200

    Added TeacherController.

---
+++ ITI.PrimarySchool/ITI.PrimarySchool.WebApp/Controllers/TeacherController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ITI.PrimarySchool.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITI.PrimarySchool.WebApp.Controllers
+{
+    [Route("/api/teacher")]
+    public class TeacherController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            //IEnumerable<TeacherData> teachers = gateway.GetAll();
+            //return Ok(teachers);
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
