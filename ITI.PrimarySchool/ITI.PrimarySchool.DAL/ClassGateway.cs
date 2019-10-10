53b84a5a2d184035ae3f01cf0264624df25bc8c4
Antoine Raquillet <antoine.raquillet@esiea.fr>
2019-10-10 09:46:25 +0200

    Added ClassController.

---
+++ ITI.PrimarySchool/ITI.PrimarySchool.DAL/ClassGateway.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ITI.PrimarySchool.DAL
+{
+    public class ClassGateway
+    {
+        public async Task<IEnumerable<ClassData>> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class ClassData
+    {
+        public int ClassId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Level { get; set; }
+    }
+}
---
+++ ITI.PrimarySchool/ITI.PrimarySchool.WebApp/Controllers/ClassController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ITI.PrimarySchool.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITI.PrimarySchool.WebApp.Controllers
+{
+    public class ClassController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            ClassGateway gateway = new ClassGateway();
+            IEnumerable<ClassData> classes = await gateway.GetAll();
+            ViewBag.Classes = classes;
+            return View();
+        }
+    }
+}
