c263d23ed17d4aeb758eec7e5d861d457515a832
Antoine Raquillet <antoine.raquillet@esiea.fr>
2019-10-14 16:04:28 +0200

    Added Counter.vue.

---
+++ ITI.PrimarySchool/ITI.PrimarySchool.Spa/src/views/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h1>Compteur</h1>
+    <p>Count: {{ count }}</p>
+    <input type="number" v-model.number="step" />
+    <button @click="onClicked">Increment</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      step: 1
+    };
+  },
+
+  methods: {
+    onClicked() {
+      this.count += this.step;
+    }
+  }
+}
+</script>
